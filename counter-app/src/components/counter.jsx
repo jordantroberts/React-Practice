a5c9a73512d233e59038b62e0630ca72cf10c8a2
Jordan Roberts <jordanroberts@talk21.com>
2019-08-07 18:05:28 +0100

    Created getBadgeClasses method to change the colour of the count button. If it's 0, it will be yellow (the bootstrap "warning" button colour) else it will be blue which is the bootstrap "primary" colour. This has been extracted to the getBadgeClasses method to prevent the render() method getting too bloated.

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -7,19 +7,28 @@ state = {
 };
  
 render() {
+   
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
+
+getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += (this.state.count === 0) ? "warning" : "primary";
+    return classes;
+    }
 
   formatCount() {
       // picking the count property from object and storing as a separate constant called count.
       const {count} = this.state; 
       return count === 0 ? "Zero" : count;
   }
+
+
 }
 
 export default Counter;
