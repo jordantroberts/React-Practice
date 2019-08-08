dfca1b66ae7feb2df927f48ef8b9369c71e46d90
Jordan Roberts <jordanroberts@talk21.com>
2019-08-08 10:26:44 +0100

    Passing an argument to handleIncrement function

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -6,15 +6,18 @@ state = {
     count: 0
 };
 
-handleIncrement = () => {
+handleIncrement = (product) => {
     this.setState({count: this.state.count+1})
-}
+};
 
 render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={this.handleIncrement}className="btn-secondary btn-sm">Increment</button>
+        <button onClick={() => this.handleIncrement(product)}
+        className="btn-secondary btn-sm"
+        >Increment
+        </button>
       </div>
     );
   }
