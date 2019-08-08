9a867212daf6ce92385dfbdddf9ad30b7cd34b94
Jordan Roberts <jordanroberts@talk21.com>
2019-08-08 09:31:14 +0100

    "Depending on our state, the renderTags method will return either paragraph or unordered list with bunch of list items"

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -7,32 +7,19 @@ state = {
     tags: ['tag1', 'tag2', 'tag3']
 };
  
+renderTags(){
+    if (this.state.tags.length === 0) return <p>There are no tags</p>; 
+    return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+}
+
 render() {
    
     return (
       <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn-secondary btn-sm">Increment</button>
-        <ul>
-            {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-        </ul>
+      {this.renderTags()}
       </div>
     );
   }
-
-getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += (this.state.count === 0) ? "warning" : "primary";
-    return classes;
-    }
-
-  formatCount() {
-      // picking the count property from object and storing as a separate constant called count.
-      const {count} = this.state; 
-      return count === 0 ? "Zero" : count;
-  }
-
-
 }
 
 export default Counter;
