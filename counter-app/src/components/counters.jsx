1b4a8773ec3a92ba438ba6f476fc9838cb885d26
Jordan Roberts <jordanroberts@talk21.com>
2019-08-08 16:47:49 +0100

    Instead of hard coding in counter components, lets add array of counter objects to state property and render using map method.

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -1,18 +1,25 @@
 import React, { Component } from "react";
 import Counter from "./counter";
+import { METHODS } from "http";
 
 class Counters extends Component {
-  state = {};
+  state = {
+      counters: [
+          {id: 1, value: 0 },
+          {id: 2, value: 0 },
+          {id: 3, value: 0 },
+          {id: 4, value: 0 },
+      ]
+  };
   render() {
     return (
       <div>
-        <Counter />
-        <Counter />
-        <Counter />
-        <Counter />
+       {this.state.counters.map(counter => <Counter key={counter.id}/>)}
       </div>
     );
   }
 }
 
 export default Counters;
+
+
