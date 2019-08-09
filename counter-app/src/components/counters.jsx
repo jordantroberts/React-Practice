eb41a483969715072adb0e399b285b347bfa0af7
Jordan Roberts <jordanroberts@talk21.com>
2019-08-09 13:16:28 +0100

    Updating the render method from the messy amount of props and just writing counter={counter} so gives everything need to know

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -4,31 +4,36 @@ import { METHODS } from "http";
 
 class Counters extends Component {
   state = {
-      counters: [
-          {id: 1, value: 5 },
-          {id: 2, value: 0 },
-          {id: 3, value: 0 },
-          {id: 4, value: 0 },
-      ]
+    counters: [
+      { id: 1, value: 5 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
   };
 
   handleDelete = counterId => {
-    // we are not going to remove a counter from that array, we create a new array, let React compare and REACT updates the state. 
+    // we are not going to remove a counter from that array, we create a new array, let React compare and REACT updates the state.
     const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({counters}); 
-    // overwriting counters property with counters constant. 
-  }; 
-  
+    this.setState({ counters });
+    // overwriting counters property with counters constant.
+  };
+
   render() {
     return (
       <div>
-       {this.state.counters.map(counter => 
-        <Counter key={counter.id} onDelete={this.handleDelete} value={counter.value} id={counter.id}/>)}
+        {this.state.counters.map(counter => (
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            counter={counter}
+            // This includes everything we need to know about the counter without the messy code. 
+          />
+          // The reason for passing both the counter.id to the key and the id is because the key is used internally for React and we can't access it by our Counter component. 
+        ))}
       </div>
     );
   }
 }
 
 export default Counters;
-
-
