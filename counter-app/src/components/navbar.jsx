4f7fa34e26e6040987eed00a2fa22a03a0d58eb8
Jordan Roberts <jordanroberts@talk21.com>
2019-08-10 16:48:54 +0100

    Destructuring if you want to get rid of lots of references to this.props

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 // convert this component into a Stateless Functional Component (using a function instead of class):
 
-const NavBar = (props) => {
+const NavBar = ({totalCounters}) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a class="navbar-brand" href="#">
         Navbar{" "}
         <span className="badge badge-pill badge-secondary">
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </a>
     </nav>
