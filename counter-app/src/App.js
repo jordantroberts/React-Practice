eb15c2e4531d6c524bf8aab9b262d0dad7d47dd4
Jordan Roberts <jordanroberts@talk21.com>
2019-08-10 08:53:15 +0100

    Lifting the state so that we can display the total number of counters on our nav bar. As there was no parent child relationship between Counters and NavBar but wanted to lift the state up to the parent App component so now both counters and navbar components have the same parents

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,18 +1,59 @@
-import React from 'react';
-import NavBar from './components/navbar';
-import './App.css';
-import Counters from './components/counters';
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import "./App.css";
+import Counters from "./components/counters";
 
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 5 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
 
-function App() {
+  handleIncrement = counter => {
+    const counters = [...this.state.counters]; // this syntax clones the array - the objects in this array are the exact same objects as the objects in counters array
+    // need to clone the modified counter at the given location so that we don't modify the state DIRECTLY (a no no in React)
+    const index = counters.indexOf(counter);
+    counter[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    // getting a new array of counters, resetting value to 0 and setting the state
+    this.setState({ counters });
+  };
+
+  handleDelete = counterId => {
+    // we are not going to remove a counter from that array, we create a new array, let React compare and REACT updates the state.
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+    // overwriting counters property with counters constant.
+  };
+
+  render() {
   return (
     <React.Fragment>
-    <NavBar />
-    <main className="container">
-      <Counters />
-    </main>
-   </React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Counters
+          counters={this.state.counters}
+          onReset={this.handleReset}
+          onIncrement={this.handleIncrement}
+          onDelete={this.handleDelete}
+        />
+      </main>
+    </React.Fragment>
   );
+
+  }
 }
 
 export default App;
--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -2,52 +2,20 @@ import React, { Component } from "react";
 import Counter from "./counter";
 import { METHODS } from "http";
 
+
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 5 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
-
-  handleIncrement = counter => {
-    const counters = [...this.state.counters]; // this syntax clones the array - the objects in this array are the exact same objects as the objects in counters array 
-    // need to clone the modified counter at the given location so that we don't modify the state DIRECTLY (a no no in React)
-    const index = counters.indexOf(counter)
-    counter[index] = {...counter}
-    counters[index].value++;
-    this.setState({counters})
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map(c => {
-        c.value = 0;
-        return c;
-    });
-    // getting a new array of counters, resetting value to 0 and setting the state
-    this.setState({counters});
-  }; 
-
-  handleDelete = counterId => {
-    // we are not going to remove a counter from that array, we create a new array, let React compare and REACT updates the state.
-    const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({ counters });
-    // overwriting counters property with counters constant.
-  };
 
   render() {
     return (
       <div>
         <button
-        onClick={this.handleReset} 
+        onClick={this.props.onReset} 
         className="btn btn-primary btn-small m-2">Reset</button>
-        {this.state.counters.map(counter => (
+        {this.props.counters.map(counter => (
           <Counter
             key={counter.id}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
             counter={counter}
             // This includes everything we need to know about the counter without the messy code.
           />
