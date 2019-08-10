11dde656b34b4764a598593c7935a9b4fdbb15af
Jordan Roberts <jordanroberts@talk21.com>
2019-08-10 16:47:15 +0100

    After changing NavBar to Stateless Functional Component, amending props to be passed as an argument (which SFC needs)

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,20 +1,18 @@
 import React, { Component } from "react";
 // convert this component into a Stateless Functional Component (using a function instead of class):
 
-const NavBar = () => {
-    return (
-        <nav className="navbar navbar-light bg-light">
-          <a class="navbar-brand" href="#">
-            Navbar <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
-          </a>
-        </nav>
-      );
+const NavBar = (props) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
 };
 
-class NavBar extends Component {
-  render() {
-    
-  }
-}
 
 export default NavBar;
